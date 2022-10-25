fa9b905f7d54cbebec19f6190eaf7b2c8f10d972
AlwaysWannaFly <15007159069@163.com>
2022-10-25 10:12:17 +0800

    初始化midway项目

---
+++ midway-project/src/config/config.default.ts
@@ -0,0 +1,9 @@
+import { MidwayConfig } from '@midwayjs/core';
+
+export default {
+  // use for cookie sign key, should change to your own and keep security
+  keys: '1666663671069_8834',
+  koa: {
+    port: 7001,
+  },
+} as MidwayConfig;
